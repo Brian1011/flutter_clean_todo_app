674ed35724d5c9ef3338e66f03c65947a2c64f9a
Brian Mutinda <mutindabrian26@gmail.com>
2024-01-18 14:30:01 +0300

    theme and router config

---
+++ lib/presentation/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'router.dart';
+
+class TodosApp extends StatelessWidget {
+  const TodosApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const color = Colors.green;
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Todos',
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+      routerDelegate: router.routerDelegate,
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: color, brightness: Brightness.light)),
+      darkTheme: ThemeData.dark().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: color, brightness: Brightness.dark)),
+      themeMode: ThemeMode.system,
+    );
+  }
+}
